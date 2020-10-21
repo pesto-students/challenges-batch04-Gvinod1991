2045d4379ef0407d58b5ce406c22179fc9248e81
Godti Vinod <venkat.godti3@gmail.com>
2020-10-21 18:00:27 +0530

    Solution to duplicate letters count problem

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,5 +1,15 @@
 function duplicateLetters(...args) {
-  return args;
+  const str = args[0];
+  const repeated = {};
+  for (let i = 0; i < str.length; i += 1) {
+    const letter = str.charAt(i);
+    if (letter in repeated) {
+      repeated[letter] += 1;
+    } else {
+      repeated[letter] = 1;
+    }
+  }
+  return Math.max(...Object.values(repeated)) > 1 ? Math.max(...Object.values(repeated)) : false;
 }
 
 export {

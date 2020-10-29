d2e908d23a66d4091f1e899530dd9cfd76f5e6d0
Godti Vinod <venkat.godti3@gmail.com>
2020-10-29 14:24:15 +0530

    Solution to sum of even args problem

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,5 +1,8 @@
-const sumEvenArgs = (...args) => {
-  return args;
-};
-
+const sumEvenArgs = (...args) => args.reduce((accumulator, current) => {
+  if (current % 2 === 0) {
+    return accumulator + current;
+  }
+  return accumulator;
+},
+0);
 export { sumEvenArgs };

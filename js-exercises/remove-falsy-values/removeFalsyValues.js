f8c53d38b120f75120864e08b6b5d2c86f662805
Godti Vinod <venkat.godti3@gmail.com>
2020-10-29 14:27:20 +0530

    Solution to remove falsy values problem

--- js-exercises/remove-falsy-values/removeFalsyValues.js
+++ js-exercises/remove-falsy-values/removeFalsyValues.js
@@ -1,6 +1,11 @@
-
 function removeFalsyValues(array) {
-
+  if (Array.isArray(array)) {
+    /* As !falsyValue is always true
+      So here truthy values are filtered out
+    */
+    return array.filter((item) => item);
+  }
+  return 'Invalid or empty array';
 }
 
 export {

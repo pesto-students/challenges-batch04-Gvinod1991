57f3cd64981d8d4d2ccad7c3c4355899667fa652
Godti Vinod <venkat.godti3@gmail.com>
2020-10-29 14:29:47 +0530

    Solution to diffArray problem to find the symetric diffrence between 2 arrays

--- js-exercises/diff-array/diffArray.js
+++ js-exercises/diff-array/diffArray.js
@@ -1,5 +1,15 @@
-function diffArray() {
-  
+function diffArray(...args) {
+  const firstArray = args[0] && Array.isArray(args[0]) ? args[0] : false;
+  const secondArray = args[1] && Array.isArray(args[1]) ? args[1] : false;
+  if (firstArray && secondArray) {
+    // filtering the first Array of element which are not in second array
+    const filterFirstArray = firstArray.filter((element) => !secondArray.includes(element));
+    // filtering the first Array of element which are not in second array
+    const filterSecondArray = secondArray.filter((element) => !firstArray.includes(element));
+    // Concatenating after filtering
+    return filterFirstArray.concat(filterSecondArray);
+  }
+  return false;
 }
 
 export {

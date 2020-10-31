799fef0ff66e2f4cef4aa3a2f2121ee30308260b
Godti Vinod <venkat.godti3@gmail.com>
2020-11-01 00:41:40 +0530

    Add new test cases for bindMethod

--- js-exercises/bind-method/bindMethod.test.js
+++ js-exercises/bind-method/bindMethod.test.js
@@ -1,4 +1,4 @@
-import { bind } from './bindMethod.js';
+import { bind} from './bindMethod.js';
 
 describe('bind method', () => {
   // Feel free to add more test cases
@@ -17,5 +17,43 @@ describe('bind method', () => {
     var y = 100;
     const bar = bind(foo, obj);
     expect(bar(10)).toBe(20);
+  });
+  test('should hard bind a function to new context object', () => {
+    const foo = function (x) {
+      return this.y + x + this.z;
+    };
+    const obj = { y: 10, z: 5 };
+    var y = 100;
+    var z = 25;
+    const bar = bind(foo, obj);
+    expect(bar(10)).toBe(25);
+  });
+  test('should hard bind a function with multiple paramter to new context ', () => {
+    const foo = function (x,y) {
+      return this.z + x + y;
+    };
+    const obj = { z: 10 };
+    var z = 100;
+    const bar = bind(foo, obj);
+    expect(bar(10,20)).toBe(40);
+  });
+  test('should hard bind works even if extra arguments passed other then function and content object', () => {
+    const foo = function (x,y,z) {
+      return this.i + x + y + z;
+    };
+    const obj = { i: 10 };
+    var i = 100;
+    const bar = bind(foo, obj,4);
+    expect(bar(10,20)).toBe(44);
+  });
+  test('should throws an error on invalid parameters', () => {
+    const foo = function (x,y,z) {
+      return this.i + x + y + z;
+    };
+    const obj = { i: 10 };
+    
+    expect(() => {bind()}).toThrow();
+    expect(() => {bind(foo,1)}).toThrow();
+    expect(() => {bind(obj,obj)}).toThrow();
   });
 });

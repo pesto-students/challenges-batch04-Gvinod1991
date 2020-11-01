930d43fe7d83305b8292bc2ba9ff9cac1007af30
Godti Vinod <venkat.godti3@gmail.com>
2020-11-01 15:24:47 +0530

    Solution to curry function

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,5 +1,16 @@
-function curry() {
-
+function curry(fn) {
+  if (typeof fn !== 'function') {
+    throw new Error(`Expected function but got ${typeof fn}`);
+  }
+  return function func(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+    return function (...args2) {
+      const argsToPass = args.concat(args2);
+      return func.apply(this, argsToPass);
+    };
+  };
 }
 
 export {

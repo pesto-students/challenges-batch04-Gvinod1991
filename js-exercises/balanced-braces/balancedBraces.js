2d9b4b9caed22bdc1f99751513994b91e7f0aa0b
Godti Vinod <venkat.godti3@gmail.com>
2020-11-01 13:10:27 +0530

    Solution to balanced braces problem which should should tell correctly whether braces opened and closed properly

--- js-exercises/balanced-braces/balancedBraces.js
+++ js-exercises/balanced-braces/balancedBraces.js
@@ -1,5 +1,27 @@
-function balancedBraces(...args) {
-  return args;
+function balancedBraces(str) {
+  if (typeof str !== 'string') {
+    throw new Error(`Expected input type String,but got the ${typeof str} `);
+  }
+  const openBraces = ['{', '(', '['];
+  const closeBraces = ['}', ')', ']'];
+  const charStack = [];
+  for (const char of str) {
+    if (openBraces.includes(char)) {
+      charStack.push(char);
+    }
+    if (closeBraces.includes(char)) {
+      const poppedChar = charStack.pop();
+      const indexOfChar = closeBraces.indexOf(char);
+      const indexOfPoppedChar = openBraces.indexOf(poppedChar);
+      if (indexOfChar !== indexOfPoppedChar) {
+        return false;
+      }
+    }
+  }
+  if (charStack.length === 0) {
+    return true;
+  }
+  return false;
 }
 
 export {

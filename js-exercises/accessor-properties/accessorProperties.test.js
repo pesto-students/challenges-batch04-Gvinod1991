406a765a546de7678b5ef0561cecd8a43bdf6ea5
Godti Vinod <venkat.godti3@gmail.com>
2020-11-04 13:34:02 +0530

    Write unit test cases for accessorProperties function

--- js-exercises/accessor-properties/accessorProperties.test.js
+++ js-exercises/accessor-properties/accessorProperties.test.js
@@ -1,7 +1,19 @@
 import { accessorProperties } from "./accessorProperties";
 
-describe("Template Test", () => {
-  test("Template Test", () => {
-    expect(true).toBe("Write your own tests here");
+describe("accessorProperties ", () => {
+  test("accessorProperties set decimal number but get should return binary number", () => {
+    const obj = accessorProperties();
+    obj.number = 36;
+    expect(obj.number).toBe(100100);
+    obj.number = 21;
+    expect(obj.number).toBe(10101);
+  });
+  test("accessorProperties should throw an error when set other then number", () => {
+    const obj = accessorProperties();
+    expect(() => obj.number = "36").toThrow();
+    expect(() => obj.number = null).toThrow();
+    expect(() => obj.number = []).toThrow();
+    expect(() => obj.number = {}).toThrow();
+    expect(() => obj.number = undefined).toThrow();
   });
 });

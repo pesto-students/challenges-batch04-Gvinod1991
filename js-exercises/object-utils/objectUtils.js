97dafaab81e3898dbf5013f457278a78759c5133
Godti Vinod <venkat.godti3@gmail.com>
2020-11-04 16:07:19 +0530

    Solution to object utilities problem

--- js-exercises/object-utils/objectUtils.js
+++ js-exercises/object-utils/objectUtils.js
@@ -1,5 +1,39 @@
-// your implementation
-
+function map(obj, callbackFunction) {
+  return Object.fromEntries(Object.entries(obj).map(([key, value]) => {
+    if (typeof value === 'object' && value !== null) {
+      return callbackFunction([key, map(value, callbackFunction)]);
+    }
+    return callbackFunction([key, value]);
+  }));
+}
+function filter(obj, callbackFunction) {
+  return Object.fromEntries(Object.entries(obj).filter(
+    ([key, value]) => callbackFunction([key, value]),
+  ));
+}
+function invert(obj) {
+  return Object.fromEntries(Object.entries(obj).map(
+    ([key, value]) => {
+      if (typeof value !== 'object') {
+        return [value, key];
+      }
+      return [key, value];
+    },
+  ));
+}
+function merge(...args) {
+  return Object.fromEntries(args.map((obj) => Object.entries(obj).flat()));
+}
+function all(obj, callbackFunction) {
+  return Object.entries(obj).reduce(
+    (acc, [key, value]) => acc && callbackFunction(key, value), true,
+  );
+}
+function some(obj, callbackFunction) {
+  return Object.entries(obj).reduce(
+    (acc, [key, value]) => acc || callbackFunction(key, value), true,
+  );
+}
 export {
   map,
   filter,

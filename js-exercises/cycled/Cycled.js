4ebe88126a4ccbd2aea666bf50d5d825459c5e6e
Godti Vinod <venkat.godti3@gmail.com>
2020-11-05 16:08:09 +0530

    Solution to cycled problem unfinished

--- js-exercises/cycled/Cycled.js
+++ js-exercises/cycled/Cycled.js
@@ -1,2 +1,26 @@
+class Cycled {
+    constructor(arr){
+        this.arr=arr;
+    }
+    *arrGenerator() {
+        for (const element of this.arr) {
+            yield element;
+        }
+    }
+    current(){
+        const gen=this.arrGenerator();
+        const result=gen.next();
+        return result.value;
+    }
+    next(){
+        let gen= this.arrGenerator();
+        let result= gen.next();
+        while(!result.done){
 
+            result=gen.next();
+        }
+        return result.value
+    }
+}
+const c = new Cycled([1,2,3]);
 export { Cycled };

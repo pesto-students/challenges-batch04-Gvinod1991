d7212860651e123c6130e0e795828b09fbf21c63
Godti Vinod <venkat.godti3@gmail.com>
2020-11-10 14:07:37 +0530

    Solution to custom allPromises problem

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,3 +1,27 @@
-const allPromises = args => {};
+const allPromises = promisesArgs => {
+  if (typeof promisesArgs === 'undefined' || promisesArgs === null) {
+    throw new Error(`Expected argument to be defined or not null but got ${typeof promisesArgs}`);
+  }
+  if (typeof promisesArgs[Symbol.iterator] !== 'function') {
+    throw new Error(`Expected argument to be iterable but got ${typeof promisesArgs}`);
+  }
+
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let completedResolves = 0;
+    promisesArgs = [].slice.call(promisesArgs);
+    promisesArgs.map((value, i) => {
+      Promise.resolve(value).then((result) => {
+        results[i] = result;
+        completedResolves += 1;
+        if (completedResolves === promisesArgs.length) {
+          resolve(results);
+        }
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  });
+};
 
 export { allPromises };
